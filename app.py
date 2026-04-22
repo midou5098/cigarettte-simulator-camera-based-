8276473a493c9ceb96c0a922ad1c7e1ac07aa726
mohamed amdouni <midouamdouni4@gmail.com>
2026-04-22 11:48:48 +0000

    added the cv2 implementation

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 from mediapipe import *
+import cv2
+
+cap=cv2.VideoCapture(0)
+if not cap :
+    print("couldnt open the camera twin")
+    exit()
+while True:
+    ret,frame=cap.read()
+    cv2.imshow("webcame",frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
